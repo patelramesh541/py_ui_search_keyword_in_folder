c286253991377317eefd8e17b0ae259bd713518e
rameshlpatel <patel.ramesh541@gmail.com>
2020-10-17 15:56:58 +0530

    added feature to create new file with entry

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from tkinter import messagebox, StringVar
+from tkinter import ttk
 import glob
 import os
+import webbrowser
 top = Tk()
 
 search_result_list = Listbox(top,width=50)
-search_result_list.grid(row=2, column=0)  
+search_result_list.grid(row=1, column=0, sticky = W, columnspan = 3, padx=10, pady=10)  
 
 file_path = "C:/search/"
 top.title("Search keywords in "+ file_path + " path")
@@ -14,7 +16,8 @@ searchValue = StringVar()
 def openFileInWindow(event):     
     cs = search_result_list.get(search_result_list.curselection())
     if "Not Found" not in cs:
-        os.system(cs)
+        #os.system(cs)
+        webbrowser.open(file_path + cs)
  
 
 
@@ -27,24 +30,49 @@ def searchContent():
         with open(file) as f:
             contents = f.read()
             if searchValue.get().lower() in file.lower() or searchValue.get().lower() in contents.lower():
-                search_result_list.insert(i+1, (file_path + str(file)))
+                search_result_list.insert(i+1, (file))
                 found = True
     #messagebox.showinfo( "Hello Python", "Hello World")
     search_result_list.bind('<Double-1>', openFileInWindow) 
     if found is False:
         search_result_list.insert(i+1, "Not Found")
+
+
+def writeFile (win, file_name, content):
+    file = open(file_path + file_name.get() + ".txt",'a+')
+    file.write(content.get("1.0", "end"))    
+    file.close() 
+    win.destroy()  
     
+ 
+def createNewEntry():    
+    win = Toplevel()
+    win.wm_title("New content in new file :")    
+    Label(win, text="File Name:",  width=50).grid(row=1,column=0, padx=10, pady=10)
+    file_name = Entry(win,  width=50)
+    file_name.grid(row=1, column=1, padx=10, pady=10)
+    content = Text(win,  width=100)
+    content.grid(row=2, column=0, sticky = W, columnspan = 2, padx=20, pady=20)
+    b = ttk.Button(win, text="Save", command=lambda: writeFile(win, file_name, content))
+    b.grid(row=3, column=0, sticky = W, columnspan = 2, padx=10, pady=10)
+   
     
+
+
+
+
 
 
 #top.bind('<Return>', searchContent)
 
 
 if __name__ == '__main__':  
-    top.geometry('400x200+100+200')
-    text_box = Entry(top, textvariable=searchValue,width=25)
-    text_box.grid(row=1, column=0) 
-    search_button = Button(top, text ="Search",width=10, command = searchContent)
-    search_button.grid(row=1, column=1)      
+    top.geometry('400x300+100+200')
+    text_box = Entry(top, textvariable=searchValue,width=30)
+    text_box.grid(row=0, column=0, padx=5, pady=5) 
+    search_button = Button(top, text ="Search",width=5, command = searchContent)
+    new_button = Button(top, text ="New",width=5, command = createNewEntry)
+    search_button.grid(row=0, column=1, padx=5, pady=5)     
+    new_button.grid(row=0,column=2, padx=5, pady=5)     
     top.mainloop()
     
